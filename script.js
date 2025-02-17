6b95026246ae474869e7397153954f09b47fef31
homan sharma <hs8146400@gmail.com>
2025-02-17 15:15:56 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,581 @@
+'use strict'
+
+// let username = 'homan'
+
+// console.log(username);
+
+// const DayInWeek = 7
+
+// alert('first press okay then see this page')
+// const conform = confirm('are you want to leave the site')
+// const value = prompt('pls enter you id')
+// console.log(value);
+
+
+// let value = 'hello'
+
+// console.log(value);
+
+// value = 100
+
+// console.log(value);
+
+// const message = 'hello'
+
+// const number = '1234'
+
+// const messages = `homan ${number.padStart(8, '*')}`
+
+// const reply = 1992233
+
+// console.log(reply.toString().padStart(8, '*'));
+
+
+// const templatestring = 'hi this is string'.concat(message)
+
+// const count = 2
+
+// const intro = `i have a ${count} laptops`
+
+// let width = prompt("enter the width");
+
+// let length = prompt("enter the length");
+
+// let area = width * length
+
+// console.log(`this is width ${width} and length ${length} `,area)
+
+// document.write(`this is width ${width} and length ${length} `,area)
+
+// userage_a = 24
+
+// userage_b = 22
+
+// user_final_age = (userage_a >= 22) && (userage_b <= 25)
+
+// document.write(user_final_age)
+
+// const firstresult = 'Hello' && console.log('hello')  
+
+// const firstresult1 = 'Hello' || console.log('hello')
+
+// preson_vot_name = 'anikat';
+
+// preson_vot_age = 19;
+
+// if (preson_vot_name = 'anikat' && preson_vot_age >=18) {
+//     document.write('preson able to give the vot');
+// }
+
+// else{
+//     document.write('person not able to give the vot');
+// }
+
+// console.log('testing');
+
+// const day = "1"  
+
+// switch (day) {
+//     case 1:
+//         console.log('Sunday');
+//         break;
+//     case "1":
+//         console.log('Sunday');
+//         break;    
+//     case 2:
+//         console.log('Monday');
+//     case 3:
+//         console.log('Tuesday');
+//     case 4:
+//         console.log('Wednesday');
+
+// }
+
+// console.log('Program Ended!!')
+
+// let users = [
+//     { id: 1, name: "Alice" },
+//     { id: 2, name: "Bob" }
+// ];
+// console.table(users);
+
+// console.log("Hello, world!"); 
+// console.log(5 + 5); 
+// let obj = { name: "John", age: 25 };
+// console.log(obj);
+
+// console.warn("This is a warning!");
+// console.info("Fetching data from API...");
+
+// const userss= [
+//     { id: 1, name:'homan', age:21},
+//     {id:2, name:'anshu',age:23}
+// ];
+// console.table(userss);
+
+// const user = 'female';
+// const output = `${user==='male'?'he':'she'} is college student.`
+
+// const name = 'homan';
+// const age = 20;
+// const graduation = false;
+
+// const username1 = '';
+// const username2 = '';
+
+// const name1 = 'ashu';
+// const name2 = 'anu';
+// const name3 = 'anshu'
+
+// const user1 = {
+//     firstname: 'homan',
+//     lastname: 'sharma',
+//     age: 22
+// }
+
+// const user2 = {
+//     firstname: 'homan',
+//     anu: 'sharma',
+//     anshu: 'anurag',
+//     age: 22,
+//     address: {
+//         city: 'mohali',
+//         area: 'ambala',
+//     }
+// }
+// // Object.seal(user2)
+
+// // user2.salary = 20000
+
+// console.log(user2.firstname);
+// console.log(user2[name2]);
+// console.log(user2[name3]);
+// console.log(user2);
+
+// Array = [['hlo','namaste','good morning'],['null','null','null'],['null','null','null']]
+// // console.log(Array[1]);
+
+
+// const x = {
+//     firstname:'anu',
+//     middlename:'mnu',
+//     lastname:'snu',
+// }
+
+// const y = {...x}
+
+// // console.log(y);
+
+// let num = 0;
+
+// while(num < 10) {
+//     console.log('hlo-hi');
+//     num = num + 1
+// }
+
+
+// let names = ['hlo','ahsh','shehj','uuedcu','uygeuy']
+// let len = names.length
+
+// let lengs = len
+// while(lengs<=10){
+//     console.log('print');
+//     lengs++
+// }
+
+// const names = ['hlo','ahsh','shehj','uuedcu','uygeuy'] 
+
+// let i = 0;
+
+// while(i < names.length){  // Loop runs while lengs is <= 10
+//     console.log(`${i+1}.${names[i]+'-procoder'}`);
+//     i++;  // Increment lengs by 1 in each iteration
+// }
+
+
+// for(i=0; i<=3; i++){
+//     console.log(i);
+//     console.log('hlo');
+//     console.log('hii');
+// }
+
+// function InterodouseMe (){
+//     console.log('hlo');
+//     console.log('hlo');
+//     console.log('hii');
+
+//     return 'ooo'
+// }
+
+
+// function InterodouseMe (age,ages){
+// console.log(`hi my name is ${username || 'mnu'}. i am ${specification || 'mechanical engineer'}. i am ${age || 26}`);
+// console.log(`this is ${username ||'procoder' }`);
+//     console.log('hii');
+
+//     return age+ages
+// }
+
+
+// InterodouseMe('homan','computer science engineer',22,22);
+
+// const value = InterodouseMe(parseInt(prompt("Enter the first age:")),parseInt(prompt("Enter the second age:")));
+// console.log(value);
+
+
+// var x = 22
+// var y = 33
+// console.log(x);
+// console.log(y);
+
+// const a = 24
+// const b = 87
+// console.log(a);
+// console.log(b);
+
+
+// sayhi()
+
+// function sayhi(){
+//     const a = 12;
+//     const b = 12;
+//     add(2,2)
+// }
+
+// function add(){
+//     const d = 3
+//     const f = 3
+//     console.log(d+f);
+// }
+
+// const anonymous = function(){ // anonymous function
+//     console.log('function');
+// }
+
+// function plus(h,l){
+//     console.log(h+l);
+// }
+
+// add()
+// anonymous()
+// plus(2,2)
+
+// num = 23;
+// console.log(num);
+
+// function mono(a){
+//     console.log(a);
+//     console.log(a)
+// }
+
+// mono(function nono(){
+//     console.log('hlo');
+// })
+
+
+// function mono(a){
+//     console.log(a);
+//     a()
+// }
+
+// mono(function nono(){
+//     console.log('hlo');
+// })
+
+
+// setTimeout run only one time 
+
+// setTimeout(`
+//     console.log("hii")
+//     console.log("homan")`,5000)
+
+// const time1 = setTimeout(`
+//     console.log("hii1")
+//     console.log("homan")`,2000)
+
+// const time2 = setTimeout(`
+//     console.log("hii2")
+//     console.log("homan")`,3000)    
+
+// const time3 = setTimeout(alfa,4000,378342,'bbidwq')     
+
+// clearTimeout(time1)    //this is uer to stop to run the setTimeout
+
+
+// function alfa(){
+//     console.log(arguments[1]);
+//     console.log('function value');
+// }
+
+
+// setInterval() this is use to repeted run and clearInterval() use to stop the clearInterval() becouse its run infinity time 
+
+
+
+// const gama = setInterval(alfa,3000,2323,'sdewqdeq')
+
+// function alfa(){
+//     console.log(arguments[1]);
+//     console.log('function value');
+// }
+
+// clearInterval(gama)  
+
+// setTimeout(`console.log("Alarm ringing! Wake up!")`,500)
+
+// setInterval(`console.log("Alarm ringing! Wake up!")`,500)
+
+// const Alarm = setInterval(`console.log("Alarm ringing! Wake up!")`,500)
+
+
+// const Alarm = setInterval(Wake_Alarm,500)
+
+// function Wake_Alarm(){
+//     console.log("Alarm ringing! Wake up!")
+// }
+
+// clearInterval(Alarm)  
+
+// const a=1
+// const b=2
+
+// function add(){
+
+//     const a=1
+//     const b=2
+
+//     function add2(){
+//         console.log(a+b);
+//     }
+//     return add2
+// }
+
+// const add3 = add()
+
+// console.log(add3);
+// add3()
+
+// const a=9
+// const b=10
+
+// function sub(){
+
+//     return (b-a)
+// }
+
+// console.log( sub());
+
+// const a = 2
+// const b = 4
+
+// const maths = {
+//     a : 'hlo',
+//     add : function (){
+//         return  (b-a)
+//     },
+//     square : function (){
+//         return a ** 20
+//     },
+//     cube (){
+//         return (a+b+2*(a*b))
+//     }
+// }
+// console.log(maths.add()+10);
+// console.log(maths.square());
+// console.log(maths.cube());
+
+
+// Function declaration
+// function greet() {
+//     console.log("Hello, world!");
+// }
+
+// Function expression
+// const greet = function() {
+//     console.log("Hello, world!");
+// };
+
+// Arrow Function expression
+// const greet = (a,b) => 
+//      a+b;
+
+// console.log(greet(2,8));
+
+// const dummy = () =>
+//     22+66;
+// console.log(dummy());
+
+// const Array = [1,2,3,4,5,6,7,8,9,10]
+
+// for(let values of Array){
+//     console.log(values);
+// }
+
+// const string = ('hlo')
+// for(let values of string){
+//     console.log(values);
+// }
+
+// const Objects = {
+//     name:'homan',
+//     address:'rajapur',
+//     age:22
+// }
+
+// for(let key in Objects){
+//     console.log(key,':',Objects[key]);
+// }
+
+// const Objectskey = Object.keys(Objects);
+// const Objectsvalues = Object.values(Objects);
+// const Objectsentries = Object.entries(Objects);
+// console.log(Objectskey);
+
+// for(const keyvalues of Objectskey){
+//     console.log(Objects[keyvalues]);
+// }
+
+
+// const Array = [1,2,3,4,5,6,7,8,9,10]
+
+// Array.forEach((Array)=>
+//     console.log(Array)
+// );
+
+
+// function add(){
+//     const Arrays = [1,2,3,4,5,6,7,8,9,10]
+//     return(Arrays)
+// }
+// console.log(add());
+
+// let fruits = ["Apple", "Banana", "Cherry", "Mango", "Orange"];
+
+// forEach return the undefind value if even i return any value
+
+// fruits.forEach((fruit,index) =>{
+//     console.log(index,fruit);
+// });
+
+// const capitalmonths = fruits.map((fruit,index) =>{
+//     console.log(index,fruit);
+//     return fruit.toUpperCase()
+// });
+
+// console.log(capitalmonths);
+
+// map return the value but forEach return the undefind value if even i return any value
+
+// console.log(fruits.map((fruit,index) =>{
+//     console.log(index,fruit.);
+//     return fruit.toUpperCase()
+// }));
+
+// in filter i able to filter the return values and this i cretae the new array 
+
+// console.log(fruits.filter((fruit,index) =>{
+//     console.log(index,fruit);
+//     return fruit.includes("a")
+// }));
+
+
+// let people = [
+//     { name: "Alice", age: 25 },
+//     { name: "Bob", age: 30 },
+//     { name: "Charlie", age: 22 },
+//     { name: "David", age: 28 }
+// ];
+
+// const persons = people.filter((peoples) => {
+//     // console.log(peoples.age > 28);
+//     return peoples.age > 28
+// })
+
+
+
+// {name: 'Alice', age: 25}         0
+// {name: 'Bob', age: 30}           1
+// {name: 'Charlie', age: 22}       2
+// {name: 'David', age: 28}         3
+
+
+// some return the true if any value is true from all if its find the one true value first the its not check the another 
+
+// const evenNumber = [0,2,4,6,8]
+
+// evenNumber.some((num,i) =>{
+//     console.log(num,i);
+// })
+
+
+// in every case if any one value is not true then its give the false 
+
+// const evenNumber = [0,2,4,6,8]
+
+// evenNumber.every((num,i) =>{
+//     console.log(num,i); 
+// })
+
+// spread opreater
+
+// const evenNumber = [0,2,4,6,8]
+
+// const oddNumber = [1,3,5,7,9]
+
+// const string = ('homan')
+
+// const Number = evenNumber.concat(oddNumber)
+
+// const Number = [...evenNumber,...oddNumber,...string]
+
+// let nums = ['homan','anu']
+
+// rest parameters 
+
+// function add(...num){
+//     console.log(num);
+//     let sum = 0;
+//     for(let i = 0; i < arguments.length; i++){
+//         sum = sum+arguments[i]
+//     }    
+//        return sum;
+// }
+
+// add(...nums)
+
+// Destructuring in JavaScript |
+
+
+let fruits = ["Apple", "Banana", "Cherry", "Mango", "Orange"];
+
+// const [,,c] = fruits
+// console.log(c);
+
+
+const user2 = {
+    firstname: 'homan',
+    age: 22,
+    address: {
+        city: 'mohali',
+        area: 'ambala',
+    }
+}
+
+// const {firstname,age,} = user2
+// console.log(age);
+// const {address:{city}} = user2
+// console.log(city);
+
+function dest({age,firstname}){
+    console.log(age,firstname);
+}
+
+dest(user2)
+
+// setTimeout(() => 
+//     location.reload(),3000
+// );
+
+
+
+// 
